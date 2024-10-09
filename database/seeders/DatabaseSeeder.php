054c2a2dac6a539a842c85f9840d7d91bdcbf587
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2024-10-09 10:38:25 +0100

    create livewire components

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        User::factory(200)->create();
+        User::factory()->count(200)->create();
 
         User::query()->inRandomOrder()->limit(10)->get()
-        ->each(fn(User $u) => Project::factory()->create(['created_by' => $u->id])
-    );
-        
+            ->each(function (User $u) {
+                $project = Project::factory()->create(['created_by' => $u->id]);
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            });
     }
 }
